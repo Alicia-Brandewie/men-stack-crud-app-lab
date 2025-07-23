3f815f5eb497415747c823b163dfcec58ea131a6
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-07-22 23:09:52 -0500

    working on catalog and display

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.cars}.`);
+  console.log(`Connected to MongoDB ${mongoose.connection.cars}.`);
 });
 
 const Car = require("./models/car.js");
@@ -18,20 +18,31 @@ app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
 
-app.get("/cars/new", (req, res)=> {
+app.get("/cars", async (req, res) => {
+  const allCars = await Car.find();
+  console.log(allCars);
+  res.render("cars/catalog.ejs", { cars: allCars });
+});
+
+app.get("/cars/new", (req, res) => {
   res.render("cars/new.ejs");
+});
+
+app.post("/cars", async (req, res) => {
+  if (req.body.currentCar === "on") {
+    req.body.currentCar = true;
+  } else {
+    req.body.currentCar = false;
+  }
+  await Car.create(req.body);
+  res.redirect("/cars");
 })
 
-app.post("/cars", async(req,res) =>{
-if (req.body.currentCar === "on") {
-  req.body.currentCar = true;
-} else {
-  req.body.currentCar = false;
-}
-await Car.create(req.body);
-  res.redirect("/cars/new");
-})
-
+app.get("/cars/:carId", async (req, res) => {
+  const foundCar = await Car.findById(req.params.carId);
+  res.render("cars/display.ejs", {car: foundCar});
+});
+//something broken here
 
 
 app.listen(3000, () => {
